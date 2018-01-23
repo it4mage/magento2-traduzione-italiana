a880cc455543aba596f4bbda600928f1c73c1fe7
Michele Fantetti <michele.fantetti@gmail.com>
2018-01-23 16:27:52 +0100

    Improvements regarding the transiction from antoniocarboni to it4mage

--- registration.php
+++ registration.php
@@ -1,7 +1,7 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::LANGUAGE,
-    'it4mage_it_it',
+    'itformage_it_it',
     __DIR__
 );
 ?>
